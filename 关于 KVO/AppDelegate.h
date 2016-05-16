9c13f4dc17345a33d0c7e8e8e61ec78eb9d0c57f
Apple <apple@promote.cache-dns.local>
2016-05-16 19:01:41 +0800

    Initial Commit

---
+++ 关于 KVO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  关于 KVO
+//
+//  Created by Apple on 16-5-16.
+//  Copyright (c) 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 关于 KVO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  关于 KVO
+//
+//  Created by Apple on 16-5-16.
+//  Copyright (c) 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
